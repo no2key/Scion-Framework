fd1f10ae5c3b63dc754fb1ab823d155f6372a036
David <emulienfou@gmail.com>
2013-10-05 15:53:58 +0200

    Remove execution time

--- index.php
+++ index.php
@@ -4,5 +4,3 @@ require 'library/Scion/Scion.php';
 
 // Instantiate a Scion application
 $app = new \Scion\Scion('config/configuration.json');
-
-echo '<br><br><strong>'.round((microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']), 3) . 'seconds</strong>';
